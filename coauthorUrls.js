2b5bbd779068ed5c41a029c666fbf7ed2181b46c
igor klep <klepigor@gmail.com>
2024-04-05 20:14:56 +0200

    First real test

---
+++ coauthorUrls.js
@@ -0,0 +1,152 @@
+function linkPovh() {
+var url1='https://scholar.google.com/citations?user=Q105AfgAAAAJ&amp;hl=en'
+document.write("<a  href=" + url1 + " target=&ldquo;blank&rdquo;>Janez Povh</a>");
+}
+function linkBurgdorf() {
+var url2= 'https://www.math.uni-konstanz.de/~burgdorf/'
+document.write("<a  href=" + url2 + " target=&ldquo;blank&rdquo;>Sabine Burgdorf</a>");
+}
+function linkHelton() {
+var url3 = 'https://www.math.ucsd.edu/~helton'
+document.write("<a  href=" + url3 + " target=&ldquo;blank&rdquo;>J. William Helton</a>");
+}
+function linkMcCullough() {
+var url4= 'https://www.math.ufl.edu/%7Esam/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Scott McCullough</a>");
+}
+function linkSpenko() {
+var url4= 'https://www.ulb.be/fr/spela-spenko'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Špela Špenko</a>");
+}
+function linkAugat() {
+var url4= 'https://www.bucknell.edu/fac-staff/meric-augat'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Meric Augat</a>");
+}
+function linkVolcic() {
+var url4= 'https://jvolcic.neocities.org/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Jurij Vol&ccaron;i&ccaron;</a>");
+}
+function linkDykema() {
+var url4= 'https://www.math.tamu.edu/~ken.dykema/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Ken Dykema</a>");
+}
+function linkVinnikov() {
+var url4= 'https://www.math.bgu.ac.il/en/people/users/vinnikov'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Victor Vinnikov</a>");
+}
+function linkSchweigh() {
+var url4= 'https://www.math.uni-konstanz.de/%7Eschweigh/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Markus Schweighofer</a>");
+}
+function linkCafuta() {
+var url4= 'https://fe.uni-lj.si/imenik-zaposlenih/kristijan-cafuta/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Kristijan Cafuta</a>");Š
+}
+function linkCimpric() {
+var url4= 'https://www.fmf.uni-lj.si/%7Ecimpric/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Jakob Cimpri&ccaron;</a>");
+}
+function linkBresar() {
+var url4= 'https://users.fmf.uni-lj.si/bresar/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Matej Brešar</a>");
+}
+function linkDolzan() {
+var url4= 'https://www.fmf.uni-lj.si/si/imenik/2995/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>David Dol&zcaron;an</a>");
+}
+function linkMoravec() {
+var url4= 'https://www.fmf.uni-lj.si/~moravec/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Primo&zcaron; Moravec</a>");
+}
+function linkUnger() {
+var url4= 'https://mathsci.ucd.ie/~tpunger/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Thomas Unger</a>");
+}
+function linkGomez() {
+var url4= 'https://www.math.cornell.edu/~gomez/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Raul Gomez</a>");
+}
+function linkVeluscek() {
+var url4= 'https://www.fmf.uni-lj.si/sl/imenik/246/veluscek-dejan/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Dejan Veluš&ccaron;ek</a>");
+}
+function linkTressl() {
+var url4= 'https://personalpages.manchester.ac.uk/staff/Marcus.Tressl/index.php'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Marcus Tressl</a>");
+}
+function linkPascoe() {
+var url3 = 'https://drexel.edu/coas/faculty-research/faculty-directory/mathematics/pascoe-james/'
+document.write("<a  href=" + url3 + " target=&ldquo;blank&rdquo;>James E. Pascoe</a>");
+}
+function linkSivic() {
+var url4= 'https://www.fmf.uni-lj.si/en/directory/231/sivic-klemen/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Klemen Šivic</a>");
+}
+function linkZalar() {
+var url4= 'https://zalara.github.io/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Aljaž Zalar</a>");
+}
+function linkHanselka() {
+var url4= 'http://www.math.uni-konstanz.de/~hanselka/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Christoph Hanselka</a>");
+}
+function linkProcesi() {
+var url4= 'http://www1.mat.uniroma1.it/people/procesi/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Claudio Procesi</a>");
+}
+function linkNie() {
+var url4= 'http://www.math.ucsd.edu/~njw/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Jiawang Nie</a>");
+}
+function linkDym() {
+var url4= 'http://www.wisdom.weizmann.ac.il/~dym/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Harry Dym</a>");
+}	
+function linkJury() {
+var url4= 'https://people.clas.ufl.edu/mjury/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Michael Jury</a>");
+}	
+function linkMagron() {
+var url4= 'https://homepages.laas.fr/vmagron/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Victor Magron</a>");
+}
+function linkHuber() {
+var url4='https://scholar.google.com.br/citations?user=1O58f3gAAAAJ&hl=en'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Felix Huber</a>");
+}
+function linkDerksen() {
+var url4='https://hderksen.sites.northeastern.edu/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Harm Derksen</a>");
+}
+function linkBeneWatts() {
+var url4='https://www.adambenewatts.ca/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Adam Bene Watts</a>");
+}
+function linkHrga() {
+var url4='https://www.fmf.uni-lj.si/sl/imenik/497/hrga-timotej/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Timotej Hrga</a>");
+}
+function linkScheiderer() {
+var url4='https://www.math.uni-konstanz.de/~scheider/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Claus Scheiderer</a>");
+}
+function linkEvert() {
+var url4='https://ericevert.com/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Eric Evert</a>");
+}
+function linkWang() {
+var url4='https://wangjie212.github.io/jiewang/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Jie Wang</a>");
+}
+function linkAraujo() {
+var url4='https://mateusaraujo.info/'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Mateus Araújo</a>");
+}
+function linkVertesi() {
+var url4='https://atomki.hu/en/users/view/192'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Tamás Vértesi</a>");
+}
+function linkNavascues() {
+var url4='https://www.iqoqi-vienna.at/people/staff/miguel-navascues'
+document.write("<a  href=" + url4 + " target=&ldquo;blank&rdquo;>Miguel Navascues</a>");
+}
